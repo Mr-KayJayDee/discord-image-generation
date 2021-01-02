2f5d1a9dada5e93d33588214224cbab81d0977a7
Simon <39917969+SimonLeclere@users.noreply.github.com>
2021-01-03 00:50:01 +0100

    Update blink.js

--- src/module/gif/blink.js
+++ src/module/gif/blink.js
@@ -1,34 +1,28 @@
 const Canvas = require(`canvas`);
-
 const GIFEncoder = require(`gifencoder`);
 
 module.exports = class Blink {
-    /**
-     * Blink
-     * @param {image} image1 
-     * @param {image} image2 
-     * @param {number} timeout 
-     */
-    async getImage(image1, image2, timeout = 1000) {
-        if (!image1) throw new Error(`You must provide an image as a first argument.`);
-        if (!image2) throw new Error(`You must provide an image as a second argument.`);
-        if (isNaN(timeout)) throw new Error(`The timeout argument must be a number.`);
-        const img = await Canvas.loadImage(image1);
-        const base = await Canvas.loadImage(image2);
-        const GIF = new GIFEncoder(480, 480);
+
+    async getImage( ...images) {
+        console.log(images)
+        if (!images || images.length < 2) throw new Error(`You must provide an image as a first argument.`);
+        
+        const GIF = new GIFEncoder(480, 480)
         GIF.start();
         GIF.setRepeat(0);
-        GIF.setDelay(timeout);
+        GIF.setDelay(1000);
         GIF.setTransparent();
+        
         const canvas = Canvas.createCanvas(480, 480);
-        const ctx = canvas.getContext(`2d`);
-        ctx.clearRect(0, 0, 480, 480);
-        ctx.drawImage(img, 0, 0, 480, 480);
-        GIF.addFrame(ctx);
-        const ctx2 = canvas.getContext(`2d`);
-        ctx2.clearRect(0, 0, 480, 480);
-        ctx2.drawImage(base, 0, 0, 480, 480);
-        GIF.addFrame(ctx2);
+
+        for (const image of images) {
+            const base = await Canvas.loadImage(image);
+            const ctx2 = canvas.getContext(`2d`);
+            ctx2.clearRect(0, 0, 480, 480);
+            ctx2.drawImage(base, 0, 0, 480, 480);
+            GIF.addFrame(ctx2);
+        }
+        
         GIF.finish();
         return GIF.out.getData();
     }
